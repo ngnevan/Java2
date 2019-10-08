25bf6748756a5821fc420a45f3dfec340059f5c7
ngnevan <n_gnevanov@mail.ru>
2019-10-08 22:36:05 +0300

    Сделал задание 7

--- src/Lesson_6/Client/Controller.java
+++ src/Lesson_6/Client/Controller.java
@@ -1,10 +1,13 @@
 package Lesson_6.Client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -13,7 +16,7 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller{
     @FXML
     TextArea textArea;
 
@@ -30,8 +33,36 @@ public class Controller implements Initializable{
     final String IP_ADRESS = "localhost";
     final int PORT = 8189;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    private boolean isAuthorized;
+
+    @FXML
+    HBox bottomPanel;
+
+    @FXML
+    HBox upperPanel;
+
+    @FXML
+    TextField loginField;
+
+    @FXML
+    PasswordField passwordField;
+
+    private void setAuthrized(boolean isAuthorized) {
+        this.isAuthorized = isAuthorized;
+        if (!isAuthorized) {
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
+        }
+    }
+
+    public void connect() {
         try {
             socket = new Socket(IP_ADRESS, PORT);
             in = new DataInputStream(socket.getInputStream());
@@ -43,8 +74,22 @@ public class Controller implements Initializable{
                     try {
                         while (true) {
                             String str = in.readUTF();
+                            if (str.startsWith("/authok")) {
+                                setAuthrized(true);
+                                break;
+                            } else {
+                                textArea.appendText(str + "\n");
+                            }
+                        }
+
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.equalsIgnoreCase("/clientClose")) {
+                                break;
+                            }
                             textArea.appendText(str + "\n");
                         }
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     } finally {
@@ -53,6 +98,7 @@ public class Controller implements Initializable{
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+                        setAuthrized(false);
                     }
                 }
             }).start();
@@ -66,6 +112,31 @@ public class Controller implements Initializable{
             out.writeUTF(textField.getText());
             textField.clear();
             textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Dispose() {
+        System.out.println("Отправляем сообщение на сервер о завершении работы");
+        try {
+            if (out != null) {
+                out.writeUTF("/end");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
